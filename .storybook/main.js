43a73cd0e763dd083dc03c71ac7a483156e2c3b0
Dave Falke <dmfalke@gmail.com>
2020-12-08 10:31:02 -0500

    Extend webpack config for plot-components

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
+const path = require('path');
 module.exports = {
   stories: ['../src/**/*.stories.js', '../src/**/*.stories.tsx'],
   addons: [
@@ -6,4 +7,13 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-knobs',
   ],
+  // Fix to allow loading external dependency as typescript. This is temporary
+  // and will be removed when this dependency is replaces with an npm package.
+  webpackFinal: async (config) => {
+    const newRule = { ...config.module.rules[0] };
+    delete newRule.exclude;
+    newRule.include = [path.resolve(__dirname, '../node_modules/visualization-tools')]
+    config.module.rules.unshift(newRule);
+    return config;
+  }
 };
